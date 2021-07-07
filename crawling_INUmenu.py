6119861a3164e01a643858d7623d516843fed264
DESKTOP-OVK9G3S\kjy03 <kjy0349@naver.com>
2021-07-07 15:37:08 +0900

    menu to csv function added

--- crawling_INUmenu.py
+++ crawling_INUmenu.py
@@ -1,16 +1,19 @@
 from bs4 import BeautifulSoup  # 크롤링을 위해 bs4 라이브러리 사용
 from urllib.request import urlopen
-import sys
+import pandas as pd
+from tqdm.notebook import tqdm
 
-print(sys.version)
-today_menu_list = [[[],[],[],[],[]]]
-not_today_menu_list = []
-for i in range(6): # 첫번째 인덱스를 요일, 두번째 인덱스를 메뉴 시간, 세번째 인덱스를 메뉴 시간별 세부 메뉴를 가진 3차원 배열
+today_menu_list = [[[], [], [], [], []]]
+all_menu_list = []
+menuTime = []
+day_list = ['월','화','수','목','금','토','일']
+tday = ""
+for i in range(6):  # 첫번째 인덱스를 요일, 두번째 인덱스를 메뉴 시간, 세번째 인덱스를 메뉴 시간별 세부 메뉴를 가진 3차원 배열
     line = []
-    not_today_menu_list.append(line)
+    all_menu_list.append(line)
     for j in range(5):
         line = []
-        not_today_menu_list[i].append(line)
+        all_menu_list[i].append(line)
 
 def find_td(selected_menu, result_list,real_index):
     index = 0
@@ -40,20 +43,38 @@ html = urlopen(url)
 soup = BeautifulSoup(html, "html.parser")  # 원하는 태그 정보를 뽑아줄 bs4 인스턴스 생성
 index = 0
 index1 = 0
-Today_served = False
 
 for tr_menu in soup.find(id='menuBox').find_all('tr'):  # menuBox라는 이름을 가진 테이블에서 한줄씩 뽑아냄
     not_today_menu = tr_menu.select('.din_list')
     today_menu = tr_menu.select('.din_lists')
+    menutime_tr = tr_menu.find(class_ = 'corn_nm')
+    day = tr_menu.find(class_ = 'din_mns')
+    if day:
+        tday = day.get_text().strip()[:1]
+        short_date = int(day.get_text().strip()[3:8].replace('/',''))
+        print(tday)
+        print(short_date)
+    if menutime_tr:
+        menuTime.append(menutime_tr.get_text())
 
     if not_today_menu:  # 찾은 내용이 있을 경우
-        find_td(not_today_menu, not_today_menu_list, index)
+        find_td(not_today_menu, all_menu_list, index)
         index += 1
 
     if today_menu:
         find_td(today_menu, today_menu_list, index1)
         index1 += 1
+for i in range(len(day_list)):
+    if tday == day_list[i]:
+        all_menu_list.insert(i,today_menu_list[0])
+        break
 
-for i in range(len(not_today_menu_list)):
-    print(not_today_menu_list[i])
-print('오늘 메뉴', today_menu_list)
+all_menu_clist = []
+for i in range(len(all_menu_list)):
+    for j in range(len(all_menu_list[i])):
+        for k in range(len(all_menu_list[i][j])):
+            temp = all_menu_list[i][j][k]
+            all_menu_clist.append(temp)
+
+menu_df = pd.DataFrame(all_menu_clist)
+menu_df.to_csv("menu.csv", header=False, index=False)
